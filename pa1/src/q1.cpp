87d94ce567ef08d2b7c9ec8ed304b664e0eae5b9
Vy Bui <johnny.bui.jb@gmail.com>
2023-01-17 23:41:06 -0800

    [pa1] fixed q1

--- pa1/src/q1.cpp
+++ pa1/src/q1.cpp
@@ -10,23 +10,22 @@ int lsa(std::vector<int> &A) {
     }
 
     int k = A.size() - 1;
-    int last_prefix_sum = 0;
+    int max_prefix_sum = std::max(0, A[k]);
     int last_largest_sum = A[k];
     k--;
-
+    
     while (k >= 0) {
-        int combined_sum = A[k] + last_largest_sum + last_prefix_sum;
-        int max_sum = std::max(A[k], last_largest_sum);
-        max_sum = std::max(max_sum, combined_sum);
+        max_prefix_sum += A[k];
+        int max_sum = std::max(max_prefix_sum, last_largest_sum);
+        max_sum = std::max(max_sum, A[k]);
         
-        if (max_sum == combined_sum) {
-            last_prefix_sum = 0;
-            last_largest_sum = combined_sum;
+        if (max_sum == max_prefix_sum) {
+            last_largest_sum = max_prefix_sum;
         } else if (max_sum == A[k]) {
-            last_prefix_sum = 0;
+            max_prefix_sum = A[k];
             last_largest_sum = A[k];
         } else {
-            last_prefix_sum += A[k];
+            max_prefix_sum = std::max(0, max_prefix_sum);
         }
 
         k--;
@@ -36,21 +35,39 @@ int lsa(std::vector<int> &A) {
 }
 
 int main() {
-    std::vector<int> in1 = {-6,12,-7,0,14,-7,5};
-    auto out1 = lsa(in1);
-    assert(out1 == 19);
+    std::vector< std::vector<int> > ins = 
+    {
+        {-6,12,-7,0,14,-7,5},
+        {2,-1,2},
+        {},
+        {-1,-2},
+        {-2,1,-3,4,-1,2,1,-5,4},
+        {1,2,-1},
+    };
 
-    std::vector<int> in2 = {2,-1,2};
-    auto out2 = lsa(in2);
-    assert(out2 == 3);
+    std::vector<int> exp = 
+    {
+        19,
+        3,
+        0,
+        -1,
+        6,
+        3
+    };
 
-    std::vector<int> in3 = {};
-    auto out3 = lsa(in3);
-    assert(out3 == 0);
+    int i = 0;
+    int from_tc = 0;
+    int to_tc = 5;
 
-    std::vector<int> in4 = {-1,-2};
-    auto out4 = lsa(in4);
-    assert(out4 == -1);
+    for (int i = from_tc; i <= to_tc; i++) {
+        auto act = lsa(ins[i]);
+
+        if (act != exp[i]) {
+            std::cout << "Test " << std::to_string(i) << " FAILS!!!" << std::endl;
+            std::cout << "Expected: " << std::to_string(exp[i]) << "\tActual: " 
+                << std::to_string(act) << std::endl;
+        }
+    }
 
     return 0;
 }
